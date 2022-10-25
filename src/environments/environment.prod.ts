8d2ad53fcdb6ff396749e6ef2a022ce269f81b20
Vivian <echeverri.vivian1120@gmail.com>
2022-10-25 10:02:18 -0500

    Despliegue en la nube

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDsEb6ktPXQOFvSKqcRM_fvJoDYUP4_3bU",
+    authDomain: "lab2-1-fba77.firebaseapp.com",
+    projectId: "lab2-1-fba77",
+    storageBucket: "lab2-1-fba77.appspot.com",
+    messagingSenderId: "301140981440",
+    appId: "1:301140981440:web:28bb1afbaf06ed22afdf25",
+    measurementId: "G-6Q6PMF58BZ"
+  }
 };
