1bd8b402c9449f72be9f1fc9bc29be8e39242ac4
Vivian <echeverri.vivian1120@gmail.com>
2022-11-06 16:14:07 -0500

    Create register Template

--- src/app/auth/create-register/create-register.component.ts
+++ src/app/auth/create-register/create-register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FirebaseErrorService } from 'src/app/services/firebase-error.service';
 
 @Component({
   selector: 'app-create-register',
@@ -6,10 +11,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-register.component.css']
 })
 export class CreateRegisterComponent implements OnInit {
+  registrarUsuario: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+    private afAuth: AngularFireAuth,
+    private toastr: ToastrService,
+    private router: Router,
+    private firebaseError: FirebaseErrorService) { 
+      this.registrarUsuario = this.fb.group({
+        apellido: ['', Validators.required],
+        nombre: ['', Validators.required],
+        email: ['', [Validators.required,Validators.email]],
+        number:['', Validators.required],
+        address:['',Validators.required],
+        age:['', Validators.required],
+        contraseña: ['', [Validators.required,Validators.minLength(7)]],
+      })
+    }
 
   ngOnInit(): void {
+  }
+
+  registrar() {
+    const apellido = this.registrarUsuario.value.apellido;
+    const nombre = this.registrarUsuario.value.nombre;
+    const email = this.registrarUsuario.value.email;
+    const contraseña = this.registrarUsuario.value.contraseña;
+
+
+    this.afAuth.createUserWithEmailAndPassword(email, contraseña).then((user) => {
+      //Si se da como exitoso el registro entonces redirecciono al Dashboard
+      this.verificarEmail();
+      //console.log(user);
+    }).catch((error) => {
+      //console.log(error);
+      this.toastr.error(this.firebaseError.codeError(error.code), 'Error');
+    })
+  }
+
+  verificarEmail(){
+    this.afAuth.currentUser.then(user=>user?.sendEmailVerification())
+                            .then(()=>{
+                              this.toastr.info('Le enviamos un correo para su verifación', 'Verificar Correo');
+                              this.router.navigate(['/Dashboard']);
+                            })
   }
 
 }
